3db017a3e219280c2549feb6a8c851a28666f951
DESKTOP-28F38VP\user <nkc.yuki.saitou@gmail.com>
2019-01-18 13:06:48 +0900

    マージ

---
+++ Assets/Script/Magic/DamageEnemy.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageEnemy : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        BaseEnemy enemy = other.GetComponent<BaseEnemy>();
+        if (enemy == null) return;
+
+        
+    }
+}
