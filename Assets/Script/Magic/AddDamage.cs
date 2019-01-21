cb4a4f665cd471f75de8f2b398330dd28e7b894f
DESKTOP-28F38VP\user <nkc.yuki.saitou@gmail.com>
2019-01-22 00:21:58 +0900

    ダメージ処理を追加

---
+++ Assets/Script/Magic/AddDamage.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddDamage : MonoBehaviour {
+
+    void OnTriggerEnter(Collider other)
+    {
+        BaseEnemy enemy = other.GetComponent<BaseEnemy>();
+        if (enemy == null) return;
+
+        enemy.EnemyHP--;
+    }
+}
